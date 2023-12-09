ef48527648f5249fc08244f5ca8c2c52ee7d86bd
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2023-12-09 17:39:05 -0500

    add base mse model

---
+++ ternary_single_test.py
@@ -0,0 +1,45 @@
+import mse_single_model
+import random
+
+Model = mse_single_model.Model
+
+# ternary inputs and outputs
+
+ternary_cases = [
+    ([0, 0, 0], 0),
+    ([0, 0, 1], 1),
+    ([0, 1, 0], 0),
+    ([0, 1, 1], 1),
+    ([1, 0, 0], 0),
+    ([1, 0, 1], 0),
+    ([1, 1, 0], 1),
+    ([1, 1, 1], 1),
+]
+
+# train model over epochs
+
+input_size = 3
+hidden_size = 6
+learning_rate = 0.02
+epochs = 20000
+model = Model.with_random_weights(input_size, hidden_size, learning_rate)
+
+log_interval = epochs // 10
+for e in range(epochs):
+    total_error = 0
+    train_cases = ternary_cases.copy()
+    random.shuffle(train_cases)
+
+    for case in train_cases:
+        hidden_output, output = model.forward(case[0])
+        output_error = model.back_prop(case[0], hidden_output, output, case[1])
+        total_error += output_error
+    
+    if (e + 1) % log_interval == 0:
+        print("epoch", e + 1, "mean error", total_error / len(train_cases))
+
+print()
+print("evaluating model:")
+for case in ternary_cases:
+    h, output = model.forward(case[0])
+    print("output", output, "expected", case[1])
